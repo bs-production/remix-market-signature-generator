67ba2f1b6f135a0d7d77383bb7404fc02ddcfb38
chrisroselli <chrisroselli@gmail.com>
2020-04-16 15:51:06 -0400

    updates

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <div class="hero-body">
         <div class="container">
           <div class="th-logo has-text-centered">
-            <img src="https://dc69b531ebf7a086ce97-290115cc0d6de62a29c33db202ae565c.ssl.cf1.rackcdn.com/2714/junkluggers-logo-upd-header.png" alt="" style="width:250px">
+            <img src="https://dc69b531ebf7a086ce97-290115cc0d6de62a29c33db202ae565c.ssl.cf1.rackcdn.com/2714/signature_jl-remix-header-logo.png" alt="" style="width:400px">
           </div>
         <h1 class="sub-text title has-text-centered">
             <p>Signature Generator</p> 
@@ -76,7 +76,7 @@
         <div class="container">
             <div class="content has-text-centered">
                 <p>
-                    &copy; 2020 The Junkluggers
+                    &copy; 2020 The Junkluggers - Remix Market
                 </p>
             </div>
         </div>
